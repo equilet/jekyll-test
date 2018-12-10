cbc42be91d2edd3ae883f35ab4cb79e4a94c75b3
Gabriel Dunne <gdunne@quilime.com>
2018-12-09 20:51:02 -0800

    Publish scripts

--- publish.sh
+++ publish.sh
@@ -2,4 +2,5 @@
 
 jekyll build
 
-rsync -avz _site/ jlubow@dabkitsch.com:/home/jlubow/dabkitsch.com/ # --delete
+# note: this blasts away the folder and replaces it with a fresh copy (--delete)
+rsync -avz _site/ jlubow@dabkitsch.com:/home/jlubow/dabkitsch.com/ --delete
